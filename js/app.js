19af48051a840e8f9fec8d4593c3ca0c8de8d12f
JeanCarlos <jmatochepascual@gmail.com>
2024-05-13 23:38:52 -0500

    [FEATURE] mostrarAlerta

--- js/app.js
+++ js/app.js
@@ -13,11 +13,9 @@ const init = (vent) => {
   // Validar formulario
 
   if (!validarFormulario()) {
-    console.log('Todos los campos son obligatorios');
+    mostrarAlerta('Error!', 'Todos los campos son obligatorios', false);
     return
   };
-
-  console.log('Formulario completado');
 };
 
 
@@ -28,6 +26,28 @@ const validarFormulario = () => {
 
   return [ciudad, pais].includes('') ? false : true;
 };
+
+
+// Mostrar alerta
+const mostrarAlerta = (initMesage, mensaje, tipo = true) => {
+  const alerta = document.querySelector('.alerta');
+
+  if (!alerta) {
+    const alertaElement = document.createElement('div');
+    alertaElement.innerHTML = `<strong class="font-bold">${initMesage}</strong>  <span>${mensaje}</span>`;
+    alertaElement.classList.add('px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center', 'alerta');
+
+    tipo ? alertaElement.classList.add('bg-green-100', 'border-green-400', 'text-green-700') : alertaElement.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
+
+    containerElement.appendChild(alertaElement);
+
+    setTimeout(() => {
+      alertaElement.remove();
+    }, 3000);
+  }
+};
+
+
 
 // Cargar Event Listeners
 document.addEventListener('DOMContentLoaded', () => {
